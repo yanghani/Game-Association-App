ee3fbadaa250a930552e71246aa04885e9f73331
yanghani <65358035+yanghani@users.noreply.github.com>
2021-07-08 01:01:41 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,13 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
+import { StartComponent } from './start/start.component';
+import { PlayComponent } from './play/play.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    PlayComponent
   ],
   imports: [
     BrowserModule,
