e9c207e28d243107ed5294ffdf98f8fd1e10e709
yanghani <65358035+yanghani@users.noreply.github.com>
2021-07-08 00:54:23 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayComponent } from './play/play.component';
+import { StartComponent } from './start/start.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: StartComponent
+  },
+  {
+    path: 'play',
+    component: PlayComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
