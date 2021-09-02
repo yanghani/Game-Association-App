8ede9bf61dc8f15dfad0925dc8cb0ad2b232c19f
yanghani <65358035+yanghani@users.noreply.github.com>
2021-09-02 13:30:10 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Association';
+  
+  next(){
+  var wordlists = [
+    'tentdress',
+    'jeans',
+    'raincoat',
+    'trolley',
+    'supermarket',
+    'beer',
+    'cigarettes',
+    'fish',
+    'sweetcorn',
+    'tomato',
+    'carrot'
+  ]
+  let easylevel = wordlists[Math.floor(Math.random()* wordlists.length)]
+  console.log(easylevel)
+  
+}
 }
