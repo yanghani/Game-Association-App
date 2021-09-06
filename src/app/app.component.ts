783ad539aac7ceb2a46baf32e06530cf3b66672e
yanghani <65358035+yanghani@users.noreply.github.com>
2021-09-06 22:02:18 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,34 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Association';
   
-  next(){
-  var wordlists = [
-    'tentdress',
-    'jeans',
-    'raincoat',
-    'trolley',
-    'supermarket',
-    'beer',
-    'cigarettes',
-    'fish',
-    'sweetcorn',
-    'tomato',
-    'carrot'
-  ]
-  let easylevel = wordlists[Math.floor(Math.random()* wordlists.length)]
-  console.log(easylevel)
+  const(){
+    let btnRandom = document.querySelector('button');
+    let result = document.querySelector('h1');
+
+    let easy = ['beer', 'dog', 'carrot', 'ocean'];
+    function getRandomNumber(min: number, max: number){
+      let step1 = max - min + 1;
+      let step2 = Math.random() * step1;
+      let result = Math.floor(step2) + min;
+
+      return result;
+    }
+
+    btnRandom.addEventListener('click', () =>{
+         let index = getRandomNumber(0, easy.length-1);
+         result.innerText = easy[index];
+    });
+  }
   
-}
+
+  
+  next(){          
+   
+  }
+  
+  
+  
+  
+  
+  
 }
